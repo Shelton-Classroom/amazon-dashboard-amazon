f0e3c7a13b819c75f8b5329bb5a9a24a06ae4786
Regina Harless <rharless6001@email.vccs.edu>
2021-03-09 12:07:51 -0500

    Update Regina

---
+++ Amazon_Dashboard/Amazon_WebApp/Amazon_WebApp/Areas/Expenses/Controllers/ExpensesController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Amazon_WebApp.Areas.Expenses.Controllers
+{
+    public class ExpensesController : Controller
+    {
+        // GET: Expenses/Expenses
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Amazon_Dashboard/Amazon_WebApp/Amazon_WebApp/Areas/Expenses/ExpensesAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace Amazon_WebApp.Areas.Expenses
+{
+    public class ExpensesAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "Expenses";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "Expenses_default",
+                "Expenses/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
