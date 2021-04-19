f23a0d5cbe0f630ad1d12b2d90a75382722efc09
rharless <rharless6001@email.vccs.edu>
2021-04-19 07:52:40 -0400

    Commit All

--- Amazon_Dashboard/Amazon_WebApp/AmazonModel/Expenses/Expenses.cs
+++ Amazon_Dashboard/Amazon_WebApp/AmazonModel/Expenses/Expenses.cs
@@ -3,10 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace AmazonModel.Expenses
 {
-    class Expenses
+    [MetadataType(typeof(ExpenseCaptureMetadata))]
+    public partial class ExpenseCapture
     {
+        private sealed class ExpenseCaptureMetadata
+        {
+            [Display(Name = "Date")]
+            public int ExpenseDate { get; set; }
+            [Display(Name = "Amount")]
+            public string ExpenseAmount { get; set; }
+            [Display(Name = "Merchant Id")]
+            public int MerchantId { get; set; }
+            [Display(Name = "Expense Code")]
+            public int ExpenseCode { get; set; }
+        }
     }
 }
--- Amazon_Dashboard/Amazon_WebApp/AmazonModel/Inventory/Inventory.cs
+++ Amazon_Dashboard/Amazon_WebApp/AmazonModel/Inventory/Inventory.cs
@@ -3,10 +3,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
 
 namespace AmazonModel.Inventory
 {
-    class Inventory
+    
+    [MetadataType(typeof(NewInventoryMetadata))]
+    public partial class NewInventory
     {
+        private sealed class NewInventoryMetadata
+        {
+            [Display(Name = "Date of Purchase")]
+            public int PurchaseDate { get; set; }
+            [Display(Name = "Inventory Type")]
+            public string InventoryType { get; set; }
+            [Display(Name = "Quantity")]
+            public int QuantityPurchased { get; set; }
+            [Display(Name = "Amount")]
+            public int Cost { get; set; }
+            [Display(Name = "Reason")]
+            public int PurchaseReason { get; set; }
+            [Display(Name = "Percentage Paid")]
+            public int PercentagePaid { get; set; }
+            [Display(Name = "Item Id")]
+            public int ItemId { get; set; }
+            [Display(Name = " Source Id")]
+            public int TheSourceId { get; set; }
+        }
     }
 }
