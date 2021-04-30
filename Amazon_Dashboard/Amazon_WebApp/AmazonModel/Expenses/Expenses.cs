a7e568f41b0f132cc5ff799324111fd97d7d9c99
rharless <rharless6001@email.vccs.edu>
2021-04-30 08:27:32 -0400

    Commit All

--- Amazon_Dashboard/Amazon_WebApp/AmazonModel/Expenses/Expenses.cs
+++ Amazon_Dashboard/Amazon_WebApp/AmazonModel/Expenses/Expenses.cs
@@ -13,11 +13,17 @@ namespace AmazonModel.Expenses
         private sealed class ExpenseCaptureMetadata
         {
             [Display(Name = "Date")]
+            [DataType(DataType.Date)]
             public int ExpenseDate { get; set; }
+
+
             [Display(Name = "Amount")]
+            [DataType(DataType.Currency)]
             public string ExpenseAmount { get; set; }
+
             [Display(Name = "Merchant Id")]
             public int MerchantId { get; set; }
+
             [Display(Name = "Expense Code")]
             public int ExpenseCode { get; set; }
         }
--- Amazon_Dashboard/Amazon_WebApp/AmazonModel/Inventory/Inventory.cs
+++ Amazon_Dashboard/Amazon_WebApp/AmazonModel/Inventory/Inventory.cs
@@ -15,19 +15,28 @@ namespace AmazonModel.Inventory
         private sealed class NewInventoryMetadata
         {
             [Display(Name = "Date of Purchase")]
+            [DataType(DataType.Date)]
             public int PurchaseDate { get; set; }
+
             [Display(Name = "Inventory Type")]
             public string InventoryType { get; set; }
+
             [Display(Name = "Quantity")]
             public int QuantityPurchased { get; set; }
+
             [Display(Name = "Amount")]
+            [DataType(DataType.Currency)]
             public int Cost { get; set; }
+
             [Display(Name = "Reason")]
             public int PurchaseReason { get; set; }
+           
             [Display(Name = "Percentage Paid")]
             public int PercentagePaid { get; set; }
+            
             [Display(Name = "Item Id")]
             public int ItemId { get; set; }
+            
             [Display(Name = " Source Id")]
             public int TheSourceId { get; set; }
         }
